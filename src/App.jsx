97d1058e71621eec581c31632422e0a1cabb9589
BecksChomoev <chomoevbeksultan@gmail.com>
2025-12-22 15:01:06 -0500

    Added the scroll to top component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import MechShop from './pages/MechShop'
 import BodyShop from './pages/BodyShop'
 import About from './pages/About'
+
+// Scroll to top on route change
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  
+  return null
+}
 
 function App() {
   const [darkMode, setDarkMode] = useState(() => {
@@ -32,6 +43,7 @@ function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <div className="min-h-screen flex flex-col">
         <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         <main className="flex-1">
